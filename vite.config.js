1a623b1ba7e6fbb46c421ef709d2bacb1509c6b3
Line-em <alineemily@hotmail.com>
2022-04-27 00:10:23 +0100

    Fix base URL to vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "https://github.com/line-em/react-weather-app/"
+	base: "/react-weather-app/"
 });
