f46211269b63a2135c33bd8b92d2200e866f0a7a
Line-em <alineemily@hotmail.com>
2022-05-16 10:51:30 +0100

    Add Loading state

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Forecast from "./components/Forecast";
 import Footer from "./components/Footer";
 import api from "./components/api";
 import axios from "axios";
+import { DotWave } from "@uiball/loaders";
 
 function App() {
 	/*Search*/
 	const [userInput, setUserInput] = useState();
+	const [isLoading, setIsLoading] = useState(false);
 
 	function updateInput(text) {
 		setUserInput(text);
@@ -27,20 +29,28 @@ function App() {
 		if (userInput) {
 			const forecastOptions = {
 				method: "GET",
+				// url: "https://localhost:8000/",
 				url: "https://api.weatherapi.com/v1/forecast.json",
 				params: { q: { userInput }, days: "3", key: api.key }
 			};
+			setIsLoading(true);
 			axios
 				.request(forecastOptions)
 				.then(function (response) {
-					setWeatherData(response.data);
-					document.body.querySelector("#searchError").style.display = "none";
-					document.body.querySelector("input").style.border = "none";
+					setTimeout(() => {
+						setIsLoading(false);
+						setWeatherData(response.data);
+						document.body.querySelector("#searchError").style.display = "none";
+						document.body.querySelector("input").style.border = "none";
+					}, 1000);
 				})
 				.catch(function (error) {
-					console.error(error);
-					document.body.querySelector("input").style.border = "var(--red) 2px solid";
-					document.body.querySelector("#searchError").style.display = "block";
+					setTimeout(() => {
+						console.error(error);
+						document.body.querySelector("input").style.border = "var(--red) 2px solid";
+						document.body.querySelector("#searchError").style.display = "block";
+						setIsLoading(false);
+					}, 1000);
 				});
 		}
 	}, [userInput]);
@@ -56,7 +66,11 @@ function App() {
 					<section className="flex-window-style">
 						<div className="main-weather-content">
 							<Search getInput={(text) => updateInput(text)} />
-
+							{isLoading && (
+								<div className="y-separator flex-center">
+									<DotWave color="#f1cef5" />
+								</div>
+							)}
 							{weatherData && (
 								<WeatherInfo
 									data={weatherData}
